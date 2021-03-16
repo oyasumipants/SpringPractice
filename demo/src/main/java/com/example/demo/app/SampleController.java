f1cf3efda0828095bfe7c5746c3071b6641950dc
Keigo Ibaraki <keigo.ibaraki@siiibo.com>
2021-03-16 13:26:23 +0900

    Connect To Database And Get Data

--- demo/src/main/java/com/example/demo/app/SampleController.java
+++ demo/src/main/java/com/example/demo/app/SampleController.java
@@ -1,5 +1,9 @@
 package com.example.demo.app;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,10 +15,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/sample")
 public class SampleController {
 
+    private final JdbcTemplate jdbcTemplate;
+
+    // Dependency Injection を使う
+    @Autowired
+    public SampleController(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     @GetMapping("/test")
     public String test(Model model) {
-        model.addAttribute("title", "Inquiry Form");
 
+        String sql = "SELECT * FROM inquiry WHERE id = 1";
+        Map<String, Object> map = jdbcTemplate.queryForMap(sql);
+
+        model.addAttribute("title", "Inquiry Form");
+        model.addAttribute("name", map.get("name"));
+        model.addAttribute("email", map.get("email"));
         return "test"; // view にデータを流す
         // 渡した値と共にHTMLにレンダリングされる
     }
